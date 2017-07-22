b319a3fee243e82e4988be9c17fe9b3ebe167b16
vagrant <vagrant@precise32.(none)>
2017-07-22 16:35:06 +0000

    setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717140933) do
+ActiveRecord::Schema.define(version: 20170722161605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20170717140933) do
     t.datetime "updated_at",        null: false
     t.string   "campaign_pic"
     t.index ["organisation_id"], name: "index_fundraisings_on_organisation_id", using: :btree
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "membershipdata", force: :cascade do |t|
@@ -69,6 +75,30 @@ ActiveRecord::Schema.define(version: 20170717140933) do
     t.string   "organisation_name"
     t.index ["email"], name: "index_organisations_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_organisations_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "Last_name"
+    t.string   "organisation_name"
+    t.integer  "organisation_id"
+    t.integer  "group_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["group_id"], name: "index_users_on_group_id", using: :btree
+    t.index ["organisation_id"], name: "index_users_on_organisation_id", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "events", "organisations"
